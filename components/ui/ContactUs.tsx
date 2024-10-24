89efa4d5d8b86c9f054b12eeff64824594cc5099
manuelExtra <olaleyeemmanuel23@gmail.com>
2024-10-24 14:33:38 +0100

    add whatsapp number - correction

--- components/ui/ContactUs.tsx
+++ components/ui/ContactUs.tsx
@@ -55,7 +55,7 @@ const ContactUs = () => {
 
       {/* <!-- WhatsApp Button --> */}
       <a
-        href='https://wa.me/+2347072182999'
+        href='https://wa.me/+2348129027941'
         className={`${SpaceGrotesk.className} mx-green-gradient text-white py-4 px-6 rounded-lg shadow-lg hover:bg-green-500 border-[#94CEA5] border`}
       >
         Let&apos;s WhatsApp
