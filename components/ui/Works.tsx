15a4b585bb7cb314d41a84ef52dcb1fef34f50d5
manuelExtra <hello@mxtechsolutions.com>
2025-10-20 16:32:16 +0100

    implement modifications on multiple file upload

--- components/ui/Works.tsx
+++ components/ui/Works.tsx
@@ -19,79 +19,59 @@ const Works = () => {
         Works we have done
       </h2>
 
-      {/* <!-- Grid container --> */}
-      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-8'>
-        {/* <!-- Card 1 --> */}
-        <div className='p-6 rounded-lg text-center'>
-          <img
-            src='/images/fantrip-project.png'
-            alt='Fantrip Screenshot'
-            className='rounded-lg mb-6 w-full'
-          />
-          <h2
-            className={`${SpaceGroteskBold.className} text-left text-xl font-bold mb-4`}
-          >
-            An airbnb-like application software for Fantrip
-          </h2>
-          <p className='text-left text-gray-400 mb-6'>
-            Fantrip is an all-in-one platform tailored for sports fans, blending
-            travel, community engagement, and exclusive sports content.
-          </p>
-          <button
-            onClick={() => workPage('https://fantrip.app')}
-            className={`${SpaceGroteskBold.className} px-6 py-3 bg-gradient-to-r from-[#0F34ED] to-pink-500 text-white font-bold  w-[100%] gradient-border rounded-lg`}
-          >
-            Check me out!
-          </button>
-        </div>
+      {/* Container */}
+      <div className='flex justify-center'>
+        <div className='flex gap-8 max-w-5xl w-full'>
+          {/* Card 1 */}
+          <div className='flex-1 p-6 rounded-lg text-center'>
+            <img
+              src='/images/doexcess.png'
+              alt='Doexcess Screenshot'
+              className='rounded-lg mb-6 w-full h-96 object-cover object-top'
+            />
+            <h2
+              className={`${SpaceGroteskBold.className} text-left text-xl font-bold mb-4`}
+            >
+              Doexcess - An autopilot for business' processes
+            </h2>
+            <p className='text-left text-gray-400 mb-6'>
+              Doexcess is an all-in-one platform that helps individuals, teams,
+              and organizations optimize tasks, streamline workflows, and work
+              more efficiently — all from a single app.
+            </p>
+            <button
+              onClick={() => workPage('https://doexcess.com')}
+              className={`${SpaceGroteskBold.className} px-6 py-3 bg-gradient-to-r from-[#0F34ED] to-pink-500 text-white font-bold w-full gradient-border rounded-lg`}
+            >
+              Check me out!
+            </button>
+          </div>
 
-        {/* <!-- Card 2 --> */}
-        <div className='p-6 rounded-lg text-center'>
-          <img
-            src='/images/bog-project.png'
-            alt='BOG Screenshot'
-            className='rounded-lg mb-6 w-full'
-          />
-          <h2
-            className={`${SpaceGroteskBold.className} text-left text-xl font-bold mb-4`}
-          >
-            A robust building service provider & e-commerce software for BOG
-          </h2>
-          <p className='text-left text-gray-400 mb-6'>
-            A platform where people can monitor and manage their projects
-            without being encumbered by time and distance.
-          </p>
-          <button
-            onClick={() => workPage('https://buildonthego.com')}
-            className={`${SpaceGroteskBold.className} px-6 py-3 bg-gradient-to-r from-[#0F34ED] to-pink-500 text-white font-bold  w-[100%] gradient-border rounded-lg`}
-          >
-            Check me out!
-          </button>
-        </div>
-
-        {/* <!-- Card 3 --> */}
-        <div className='p-6 rounded-lg text-center'>
-          <img
-            src='/images/mywishlistng-project.png'
-            alt='Mywishlisting Screenshot'
-            className='rounded-lg mb-6 w-full'
-          />
-          <h2
-            className={`${SpaceGroteskBold.className} text-left text-xl font-bold mb-4`}
-          >
-            Ecommerce web software for Mywishlistng
-          </h2>
-          <p className='text-left text-gray-400 mb-6'>
-            Mywishlistng is a gift registry that allows you to choose the ideal
-            gifts for your special occasion.
-          </p>
-
-          <button
-            onClick={() => workPage('https://mywishlistng.com')}
-            className={`${SpaceGroteskBold.className} px-6 py-3 bg-gradient-to-r from-[#0F34ED] to-pink-500 text-white font-bold  w-[100%] gradient-border rounded-lg`}
-          >
-            Check me out!
-          </button>
+          {/* Card 2 */}
+          <div className='flex-1 p-6 rounded-lg text-center'>
+            <img
+              src='/images/cva.png'
+              alt='CVA Screenshot'
+              className='rounded-lg mb-6 w-full h-96 object-cover object-top'
+            />
+            <h2
+              className={`${SpaceGroteskBold.className} text-left text-xl font-bold mb-4`}
+            >
+              Christ’s Victorious Army — A Believers’ Community
+            </h2>
+            <p className='text-left text-gray-400 mb-6'>
+              Christ’s Victorious Army is a word-based, full-gospel Christian
+              ministry—interdenominational and non-denominational—committed to
+              honoring God and helping believers grow spiritually and live in
+              wholehearted service to Him.
+            </p>
+            <button
+              onClick={() => workPage('https://christsvictoriousarmy.org')}
+              className={`${SpaceGroteskBold.className} px-6 py-3 bg-gradient-to-r from-[#0F34ED] to-pink-500 text-white font-bold w-full gradient-border rounded-lg`}
+            >
+              Check me out!
+            </button>
+          </div>
         </div>
       </div>
     </section>
