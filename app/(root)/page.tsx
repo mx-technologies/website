1af7ea0807311c20c4f9bad44eb893fa1257946d
manuelextra <olaleyeemmanuel23@gmail.com>
2026-01-31 11:40:26 +0100

    implement website modifications

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,5 @@
 import AboutUs from '@/components/ui/AboutUs';
 import ContactUs from '@/components/ui/ContactUs';
-import Footer from '@/components/ui/Footer';
 import Services from '@/components/ui/Services';
 import TechStack from '@/components/ui/TechStack';
 import WhyChooseUs from '@/components/ui/WhyChooseUs';
