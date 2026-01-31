d4b3809c01718d7f90f85bef2019b83304b37e65
manuelextra <olaleyeemmanuel23@gmail.com>
2026-01-31 13:31:16 +0100

    implement data transparency removal

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -16,7 +16,6 @@ export default function Home() {
       <Works />
       {/* <Testimonials /> */}
       <AboutUs />
-      <DataTransparency />
       <ContactUs />
       {/* <Footer /> */}
     </main>
